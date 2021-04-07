783ab8a7b0da7c010dd47944287c7a354ced6572
Zach Merrill <zemerrill@gmail.com>
2021-04-07 13:53:10 -0400

    Started building plant view

--- Twig/ViewControllers/PlantViewController.swift
+++ Twig/ViewControllers/PlantViewController.swift
@@ -11,13 +11,38 @@ class PlantViewController: UIViewController {
     
     // MARK: Properties
     private var plant:String?
+    
+    // MARK: Outlets
+    @IBOutlet weak var waterButton: UIButton!
+    @IBOutlet weak var notificationLabel: UITextView!
+    @IBOutlet weak var quickAddButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Setup view
         self.title = plant
+        
+        // Setup quick add button to be a dropdown
+        quickAddButton.menu = UIMenu(title: "", children: quickAddMenuActions())
+        quickAddButton.showsMenuAsPrimaryAction = true
+        
+        // Setup notification label
+        notificationLabel.isHidden = true // Hide until it's almost watering day
+        
+        // Setup water button
+        waterButton.isHidden = true // Hide until it's almost watering day
+        waterButton.layer.cornerRadius = 5.0
+
     }
+    
+    private func quickAddMenuActions() -> [UIAction] {
+        return [
+            // Add plant menu action
+            UIAction(title: "Add Another", image: UIImage(systemName: "plus"), identifier: nil, discoverabilityTitle: nil, attributes: .init(), state: .off, handler: { _ in })
+        ]
+    } // quickAddMenuActions
     
     @IBAction func deletePlant(_ sender: Any) {
         Plant.delete(name: plant!)
