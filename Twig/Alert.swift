0a766c6991e26fcd104a3cf3f187111aaf876695
Zach Merrill <zemerrill@gmail.com>
2021-04-12 20:50:41 -0400

    Replace text with placeholder

--- Twig/Alert.swift
+++ Twig/Alert.swift
@@ -13,14 +13,14 @@ class Alert {
     class func addRoomAndAlert(controller: UIViewController){
         let alertTitle = "Add Room"
         let alertMessage = "Room name:"
-        let textFieldText = "Bedroom"
+        let placeholder = "eg. Bedroom"
         let addButtonTitle = "Add"
         let cancelButtonTitle = "Cancel"
         
         // Creates a simple alert to add a room
         let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
         alert.addTextField(configurationHandler: { (nameTextField) -> Void in
-            nameTextField.text = textFieldText
+            nameTextField.placeholder = placeholder
         })
         alert.addAction(UIAlertAction(title: addButtonTitle, style: .default, handler: { [weak alert] (action) -> Void in
             let nameTextField = (alert?.textFields![0])! as UITextField
