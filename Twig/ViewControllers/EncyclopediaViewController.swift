634a7db97da6a477a0fae329b3db0efbe1ceff06
Zach Merrill <zemerrill@gmail.com>
2021-04-18 11:31:27 -0400

    Fix incorrect plant selection & table reload & add

--- Twig/ViewControllers/EncyclopediaViewController.swift
+++ Twig/ViewControllers/EncyclopediaViewController.swift
@@ -14,38 +14,43 @@ class EncyclopediaViewController: UIViewController, UITableViewDataSource, UITab
     @IBOutlet weak var searchBar: UISearchBar!
     private let context = AppDelegate.viewContext
     let segueId = "PlantViewSegue"
+    private let addPlantIdentifier = "AddPlantSegueIdentifier"
     var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
     var plantsFiltered = [Plant]()
     
+    override func viewWillAppear(_ animated: Bool) {
+        print("VIEW APPEARED")
+        plantsFiltered = Plant.getAllPlants()!
+        tableView.reloadData()
+    }
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-            initializeFetchedResultsController()
-            plantsFiltered = Plant.getAllPlants()!
-        }
-        // MARK: NSFetchedResultsController Functions
-        func initializeFetchedResultsController() {
-            let request : NSFetchRequest<Plant> = Plant.fetchRequest()
-            let fetchSort = NSSortDescriptor(key: "name", ascending: true)
-            request.sortDescriptors = [fetchSort]
-            
-            fetchedResultsController = NSFetchedResultsController(
-                fetchRequest: request,
-                managedObjectContext: context,
-                sectionNameKeyPath: nil,
-                cacheName: nil) as? NSFetchedResultsController<NSFetchRequestResult>
-            
-            fetchedResultsController.delegate = self
-            do {
-                try fetchedResultsController.performFetch()
-            } catch {
-                fatalError("Failed to initialize FetchedResultsController: \(error)")
-            }
-        }
+        super.viewDidLoad()
+        initializeFetchedResultsController()
+        plantsFiltered = Plant.getAllPlants()!
+    }
     
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return plantsFiltered.count
+    // MARK: NSFetchedResultsController Functions
+    func initializeFetchedResultsController() {
+        let request : NSFetchRequest<Plant> = Plant.fetchRequest()
+        let fetchSort = NSSortDescriptor(key: "name", ascending: true)
+        request.sortDescriptors = [fetchSort]
+        
+        fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil) as? NSFetchedResultsController<NSFetchRequestResult>
+        
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
         }
+    }
+    
+    // MARK: TableView
     //Filerts the table views array
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         plantsFiltered = Plant.getAllPlants()!
@@ -56,28 +61,36 @@ class EncyclopediaViewController: UIViewController, UITableViewDataSource, UITab
         )
         tableView.reloadData()
     }
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            var cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.customCellIdentifier, for: indexPath) as? CustomCell
-            if (cell == nil) {
-                cell = CustomCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: CustomCell.customCellIdentifier)
-            }
-            let plant = plantsFiltered[indexPath.row]//fetchedResultsController.object(at: indexPath) as! Plant
-            // fill cells question
-            cell?.cellText?.text = plant.name
-            return cell!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return plantsFiltered.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.customCellIdentifier, for: indexPath) as? CustomCell
+        if (cell == nil) {
+            cell = CustomCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: CustomCell.customCellIdentifier)
         }
+        let plant = plantsFiltered[indexPath.row]
+        // fill cells plant name
+        cell?.cellText?.text = plant.name
+        return cell!
+    }
+    
+    //Start segue to the view card view
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let plant = plantsFiltered[indexPath.row]
+        performSegue(withIdentifier: segueId, sender: plant)
+    }
+    
+    // MARK: Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == segueId{
             let detailVc = segue.destination as! PlantViewController
             detailVc.initWithPlantNamed((sender as! Plant).name ?? "Undefined")
         }
     } // prepareForSegue
-    //Start segue to the view card view
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let plant = fetchedResultsController.object(at: indexPath) as! Plant
-        performSegue(withIdentifier: segueId, sender: plant)
-    }
 }
 
 
