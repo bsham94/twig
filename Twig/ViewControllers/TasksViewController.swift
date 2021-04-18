297be1c23064f908edeabd48832cad3e749a3146
Zach Merrill <zemerrill@gmail.com>
2021-04-18 13:10:20 -0400

    Removed some junk

--- Twig/ViewControllers/TasksViewController.swift
+++ Twig/ViewControllers/TasksViewController.swift
@@ -10,18 +10,13 @@ import CoreData
 
 class TasksViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, NSFetchedResultsControllerDelegate {
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var searchBar: UISearchBar!
     private let context = AppDelegate.viewContext
     let segueId = "PlantViewSegue"
     var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
-    var plantsFiltered = [Plant]()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeFetchedResultsController()
-        plantsFiltered = Plant.getAllPlants()!
     }
     
     // MARK: NSFetchedResultsController Functions
@@ -45,26 +40,20 @@ class TasksViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return plantsFiltered.count
+        if let sections = fetchedResultsController?.sections, sections.count > 0 {
+            return sections[section].numberOfObjects
+        } else {
+            return 0
+        }
     }
     
-    //Filerts the table views array
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        plantsFiltered = Plant.getAllPlants()!
-        .filter({ plant -> Bool in
-                if searchText.isEmpty { return true }
-                return plant.name!.lowercased().contains(searchText.lowercased())
-            }
-        )
-        tableView.reloadData()
-    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.customCellIdentifier, for: indexPath) as? CustomCell
         if (cell == nil) {
             cell = CustomCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: CustomCell.customCellIdentifier)
         }
-        let plant = plantsFiltered[indexPath.row]//fetchedResultsController.object(at: indexPath) as! Plant
-        // fill cells question
+        let plant = fetchedResultsController.object(at: indexPath) as! Plant
+        // fill cells plant name
         cell?.cellText?.text = plant.name
         return cell!
     }
